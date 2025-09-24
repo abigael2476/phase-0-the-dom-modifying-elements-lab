9d6239b92fe0c42e2b455ed1c105321afc821d1b
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-09-25 00:12:18 +0300

    added headers

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// 1. Remove the <main id="main"> node
+const main = document.getElementById("main");
+main.remove();
+
+// 2. Create a new <h1> and assign it to newHeader
+const newHeader = document.createElement("h1");
+
+// 3. Give it an id of 'victory'
+newHeader.id = "victory";
+
+// 4. Set its text
+newHeader.textContent = "Abigael is the champion"; // <-- replace Abigael with your name
+
+// 5. Append it to the document body
+document.body.append(newHeader);
